237eb7f5399268130e99c7da8634ac69f1d64683
Vigil <104719182+VIGILLANCES@users.noreply.github.com>
2026-08-05 11:16:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def home():
 
 @app.route("/webhook", methods=["GET"])
 def verify():
+    mode = request.args.get("hub.mode")
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
 
-    if token == VERIFY_TOKEN:
+    if mode == "subscribe" and token == VERIFY_TOKEN:
         return challenge, 200
-
     return "Verification failed", 403
 
 @app.route("/webhook", methods=["POST"])
@@ -23,3 +23,6 @@ def webhook():
     data = request.get_json()
     print(data)
     return "EVENT_RECEIVED", 200
+
+if __name__ == "__main__":
+    app.run()
